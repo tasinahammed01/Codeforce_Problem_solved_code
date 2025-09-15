02ad2db4ebd075485d113173f64b7838144eea7c
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-09-15 22:47:42 +0600

    setup completed

---
+++ Boy or girl/boyOrGirl.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    string name;
+    cin >> name;
+
+
+
+     if(((name.()) % 2 != 0)){
+        cout << "CHAT WITH HER!";
+    }
+    else{
+
+        cout << "IGNORE HIM!";
+    }
+
+}
